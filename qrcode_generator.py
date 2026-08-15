bb5f167ca400a8c71a5b70fde48030fb4df5e648
Logeshwaran B <logeshwaran.abc1299@gmail.com>
2026-08-15 14:32:06 +0530

    Fix QR code Render URL

--- qrcode_generator.py
+++ qrcode_generator.py
@@ -13,9 +13,10 @@ def generate_qr_code(certificate_id):
     )
 
     verification_url = (
-        f"https://blockchain-certificate-verification-dkt5.onrender.com"
-        f"/certificate/{certificate_id}"
-    )
+    f"https://certificate-verification-system-1i3al.onrender.com"
+    f"/certificate/{certificate_id}"
+)
+    
 
     qr.add_data(verification_url)
     qr.make(fit=True)
